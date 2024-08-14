9530cbd6adfa3738c4b5471e2823f02a648f76f8
Sreelakshmi2008 <sreetb2008@gmail.com>
2024-08-14 23:49:25 +0530

    added a new endpoint

--- app/views.py
+++ app/views.py
@@ -230,8 +230,7 @@ def opportunities_webhook(request):
                                 opp_instance.how_many_times_lender_change = field['fieldValue']
 
                             if field['id'] == "TQXTPRZqpXKMy9aaP42A":
-                                date_str = field['fieldValue']
-                                opp_instance.close_due_date = date_str.date()
+                                opp_instance.close_due_date = field['fieldValue']
 
                 opp_instance.save()
 
